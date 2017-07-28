3fc5617947d9e39d817f841342ba05cf131fd0a5
mbshqqb <mbshqqb@qq.com>
2017-07-28 15:33:39 +0800

    Merge branch 'master' of Z:\PublicSentimentAnalysis\DataSourceWorkSpace\spider-python\SpiderWeiBo with conflicts.

--- com/weibo_com/UserWeiBo.py
+++ com/weibo_com/UserWeiBo.py
@@ -8,6 +8,6 @@ import datetime
 user_ids=get_user_id()
 def main():
     for user_id in user_ids:
-        if get_user_weibos(user_id,datetime.datetime.now()- datetime.timedelta(hours=50),"weibo_info") is False:
+        if get_user_weibos(user_id,datetime.datetime.now()- datetime.timedelta(hours=30),"weibo_info") is False:
             break
 main()
